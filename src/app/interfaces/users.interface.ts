31d26b6ab6dcdeb114bfdbf0a23d2e5c29b44e44
QuimeyDAgos <164278589+quimey2312@users.noreply.github.com>
2024-10-31 08:52:27 -0300

    update interfaz usuario

--- src/app/interfaces/users.interface.ts
+++ src/app/interfaces/users.interface.ts
@@ -4,5 +4,11 @@ export interface Usuario {
         username: string;
         password: string;
         role: 'admin' | 'basic' | 'premium'; // Enum para roles
+        nombre:string;
+        email: string;
+        peso: number;
+        altura:number;
+        proceso:string;
+        nutricion: string;
 
 }
