8f042bed1080d61def8788ffda142f21fd33217a
QuimeyDAgos <164278589+quimey2312@users.noreply.github.com>
2024-11-03 15:38:26 -0300

    Actualizado Contact Component

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -8,13 +8,23 @@ import { Component } from '@angular/core';
 export class ContactComponent {
   private phoneNumber: string = '542235999999';
   public whatsappLink: string = '';
+  private instaUser: string ='quimey_dagos';
+  public instagramLink: string = '';
+
   public base64Image: string = '';
 
   constructor() {
     this.generateWhatsAppLink();
+    this.setInstagramLink('tu_usuario');
   }
 
   private generateWhatsAppLink(): void {
     this.whatsappLink = `https://wa.me/${this.phoneNumber}`;
+  }
+
+
+
+  setInstagramLink(username: string): void {
+    this.instagramLink = `https://instagram.com/${this.instaUser}`;
   }
 }
