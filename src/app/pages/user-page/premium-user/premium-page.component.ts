1e24bc3cd77312b354d9187ea0f2bb85c83ae60c
Santiago Granone <santigranone@gmail.com>
2024-11-14 16:27:05 -0300

    Responsive / Box Whatsapp Rutina

--- src/app/pages/user-page/premium-user/premium-page.component.ts
+++ src/app/pages/user-page/premium-user/premium-page.component.ts
@@ -3,20 +3,28 @@ import { CalendarComponent } from '../calendar/calendar.component';
 import { RoutineListComponent } from '../../routine-page/routine-list/routine-list.component';
 import { UsuarioService } from '../../../services/usuario.service';
 
-
 @Component({
   selector: 'app-premium-page',
   standalone: true,
   imports: [CalendarComponent, RoutineListComponent],
   templateUrl: './premium-page.component.html',
-  styleUrl: './premium-page.component.css'
+  styleUrl: './premium-page.component.css',
 })
 export class PremiumPageComponent {
-
   nombreUsuario: string | null = null;
 
-
   constructor(private usuarioService: UsuarioService) {}
+
+  private phoneNumber: string = '542235999999';
+  public whatsappLink: string = '';
+  public message: string = '';
+
+  sendMessage(message: string): void {
+    const whatsappLink = `https://web.whatsapp.com/send?phone=${
+      this.phoneNumber
+    }&text=${encodeURIComponent(message)}`;
+    window.open(whatsappLink, '_blank');
+  }
 
   ngOnInit(): void {
     this.usuarioService.getUsuarioActual().subscribe({
@@ -26,9 +34,7 @@ export class PremiumPageComponent {
       error: (err) => {
         console.error('Error al obtener el usuario:', err);
         this.nombreUsuario = 'Usuario'; // Valor por defecto si ocurre un error
-      }
+      },
     });
   }
-
-
 }
