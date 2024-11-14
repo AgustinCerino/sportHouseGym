a6cd577e2f850e5692e13ebe8d52e8f1f02b4c06
abrumay <abrumay@hotmail.com>
2024-11-14 18:05:01 -0300

    FRONT prem-page basic-page

--- src/app/pages/user-page/basic-user/basic-page.component.ts
+++ src/app/pages/user-page/basic-user/basic-page.component.ts
@@ -3,16 +3,27 @@ import { Component, OnInit } from '@angular/core';
 import { CalendarComponent } from '../calendar/calendar.component';
 import { RoutineListComponent } from '../../routine-page/routine-list/routine-list.component';
 import { UsuarioService } from '../../../services/usuario.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-basic-page',
   standalone: true,
-  imports: [CalendarComponent, RoutineListComponent],
+  imports: [CalendarComponent, RoutineListComponent, CommonModule],
   templateUrl: './basic-page.component.html',
   styleUrls: ['./basic-page.component.css']
 })
 export class BasicPageComponent implements OnInit {
   nombreUsuario: string | null = null;
+  images: string[] = [
+    'icons/page1.jpg',
+    'icons/page2.webp',
+    'icons/page3.jpg',
+    'icons/page4.jpg'
+  ];
+
+  currentImageIndex: number = 0;
+  imageWidth: number = 600; // Establecer el ancho de las imágenes
+  translateValue: number = 0; // Desplazamiento para las imágenes
 
   constructor(private usuarioService: UsuarioService) {}
 
@@ -26,7 +37,34 @@ export class BasicPageComponent implements OnInit {
         this.nombreUsuario = 'Usuario'; // Valor por defecto si ocurre un error
       }
     });
+
+     
+  }
+
+  prevImage() {
+    if (this.currentImageIndex > 0) {
+      this.currentImageIndex--;
+    } else {
+      this.currentImageIndex = this.images.length - 1;
+    }
+    this.updateTranslateValue();
+  }
+
+  // Método para mostrar la imagen siguiente
+  nextImage() {
+    if (this.currentImageIndex < this.images.length - 1) {
+      this.currentImageIndex++;
+    } else {
+      this.currentImageIndex = 0;
+    }
+    this.updateTranslateValue();
+  }
+
+  // Actualiza el desplazamiento de las imágenes
+  updateTranslateValue() {
+    this.translateValue = -this.currentImageIndex * this.imageWidth;
   }
 
 }
+
 
--- src/app/pages/user-page/premium-user/premium-page.component.ts
+++ src/app/pages/user-page/premium-user/premium-page.component.ts
@@ -2,16 +2,27 @@ import { Component } from '@angular/core';
 import { CalendarComponent } from '../calendar/calendar.component';
 import { RoutineListComponent } from '../../routine-page/routine-list/routine-list.component';
 import { UsuarioService } from '../../../services/usuario.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-premium-page',
   standalone: true,
-  imports: [CalendarComponent, RoutineListComponent],
+  imports: [CalendarComponent, RoutineListComponent, CommonModule],
   templateUrl: './premium-page.component.html',
   styleUrl: './premium-page.component.css',
 })
 export class PremiumPageComponent {
   nombreUsuario: string | null = null;
+  images: string[] = [
+    'icons/page1.jpg',
+    'icons/page2.webp',
+    'icons/page3.jpg',
+    'icons/page4.jpg'
+  ];
+
+  currentImageIndex: number = 0;
+  imageWidth: number = 600; // Establecer el ancho de las imágenes
+  translateValue: number = 0; // Desplazamiento para las imágenes
 
   constructor(private usuarioService: UsuarioService) {}
 
@@ -36,5 +47,30 @@ export class PremiumPageComponent {
         this.nombreUsuario = 'Usuario'; // Valor por defecto si ocurre un error
       },
     });
+  }
+
+
+  prevImage() {
+    if (this.currentImageIndex > 0) {
+      this.currentImageIndex--;
+    } else {
+      this.currentImageIndex = this.images.length - 1;
+    }
+    this.updateTranslateValue();
+  }
+
+  // Método para mostrar la imagen siguiente
+  nextImage() {
+    if (this.currentImageIndex < this.images.length - 1) {
+      this.currentImageIndex++;
+    } else {
+      this.currentImageIndex = 0;
+    }
+    this.updateTranslateValue();
+  }
+
+  // Actualiza el desplazamiento de las imágenes
+  updateTranslateValue() {
+    this.translateValue = -this.currentImageIndex * this.imageWidth;
   }
 }
